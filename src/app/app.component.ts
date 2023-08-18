d2263e69c426dbdc0c84e849dfb02928860e5ffe
Markosgt7 <markos.guate7@gmail.com>
2023-08-18 15:42:16 -0600

    configuracion del service y del app.component.ts y interpolacion de variables en la vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { WeatherService } from './services/weather.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'AppClima-RapidApi';
+  temperatura: number = 0;
   min: number = 0;
   max: number = 0;
   viento: number = 0;
@@ -15,17 +16,23 @@ export class AppComponent implements OnInit{
   clima: any;
   icono: string = '';
   ciudad_consulta: string = '';
+  nombre_ciudad: string = '';
 
+  constructor(private weatherService: WeatherService) {}
 
-  constructor(private weatherService: WeatherService){}
-
-  ngOnInit():void{
-    this.weatherService.getWeather('guatemala').subscribe({
-      next:(response)=>{
-        console.log(response);  
-
+  ngOnInit(): void {
+    this.weatherService.getWeather('lima').subscribe({
+      next: (response) => {
+        console.log(response);
+        this.clima = response;
+        this.min = this.clima.main.temp_min;
+        this.max = this.clima.main.temp_max;
+        this.humedad = this.clima.main.humidity;
+        this.viento = this.clima.wind.speed;
+        this.nombre_ciudad = this.clima.name;
+        this.temperatura = this.clima.main.temp;
       },
-      error: (err) => console.info('Error',err.message),
-    })
+      error: (err) => console.info('Error', err.message),
+    });
   }
 }
